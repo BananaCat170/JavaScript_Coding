cdbcf08b842227ab7d52dafad371bca4f579f3eb
BananaCat170 <aaronmaines69@gmail.com>
2024-07-17 14:38:26 -0400

    commit #2!!

---
+++ story_time.js
@@ -0,0 +1,31 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let name = "Aaron";
+let world = "Kerpleckischalpligrupick";
+let catName = "Krapogu";
+let cityName = "The Grand, Amazing, And Quite Spectacular City of Ham, The Name of Which Must Be Spoken in Full Every Time Or Else You Will Go To Jail";
+
+// TODO: Create 3-Integer variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 17;
+let cityPopulation = 12;
+let numberOfGoats = 3677;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let items = ["Haminator", "ID card", "Stick"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let currentlyRidingAnElephantIntoWar = false;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("Yo, what up, my name is " + name + ".");
+console.log("I am " + age + " years old. I have a cat named " + catName + ".");
+console.log("I live in " + cityName + ", which is in the world of " + world + ".");
+console.log("The population of " + cityName + " is " + cityPopulation + ".");
+console.log("I am a goat farmer and I currently have " + numberOfGoats + " goats in my possession.");
+console.log("I have many items, including a " + items[0] + ", an " + items[1] + ", and a " + items[2] + ".");
+console.log("People always ask me whether or not I am riding an elephant into war. Most of the time, that tends to be " + currentlyRidingAnElephantIntoWar + ".");
+console.log("Anyways, I'm off to ride an elephant into war. Bye!");
